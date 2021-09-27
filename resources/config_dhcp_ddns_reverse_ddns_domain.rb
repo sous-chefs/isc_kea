ec74fd646be14ce3128629e72eff7e16629c982e
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2021-09-27 20:27:06 +0100

    Add DhcpDdns resources

---
+++ resources/config_dhcp_ddns_reverse_ddns_domain.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook:: isc_kea
+# Resource:: config_dhcp_ddns_reverse_ddns_domain
+#
+# Copyright:: Ben Hughes <bmhughes@bmhughes.co.uk>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+unified_mode true
+
+use 'partial/_config_auto_accumulator'
+use 'partial/_config_parameters_common'
+
+def auto_accumulator_options_override
+  {
+    config_path_override: %w(DhcpDdns reverse-dns ddns-domains),
+    config_path_type: :array,
+    config_path_match_field: 'name',
+    config_path_match_value: zone_name,
+    property_translation_matrix: {
+      zone_name: 'name',
+    },
+  }.freeze
+end
+
+property :zone_name, String
+
+property :key_name, String
+
+property :dns_servers, Hash
---
+++ resources/config_dhcp_ddns_tsig_key.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook:: isc_kea
+# Resource:: config_dhcp_ddns_tsig_key
+#
+# Copyright:: Ben Hughes <bmhughes@bmhughes.co.uk>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+unified_mode true
+
+use 'partial/_config_auto_accumulator'
+use 'partial/_config_parameters_common'
+
+def auto_accumulator_options_override
+  {
+    config_path_override: %w(DhcpDdns tsig-keys),
+    config_path_type: :array,
+    config_path_match_field: 'name',
+    config_path_match_value: key_name,
+    property_translation_matrix: {
+      key_name: 'name',
+    },
+  }.freeze
+end
+
+property :key_name, String
+
+property :algorithm, String,
+          equal_to: %w(HMAC-MD5 HMAC-SHA1 HMAC-SHA224 HMAC-SHA256 HMAC-SHA386 HMAC-SHA512),
+          coerce: proc { |p| p.upcase }
+
+property :digest_bits, Integer
+
+property :secret, String
